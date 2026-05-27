c82d98bc6d0f56616e0a2dec85f6008321c5f699
zhigunovsemyon <zhigunovsemen@gmail.com>
2026-05-27 16:22:07 +0300

    Накидал функций в Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,31 @@
 ﻿using CS_DHT;
 
-var benchmarks = Benchmarks.BuildNetworks(10_000);
-Console.WriteLine("Сети построены");
+static void tests()
+{
+	Console.Clear();
+	throw new NotImplementedException("tests()");
+}
 
-benchmarks.RunBenchmarks(100_000);
-Console.Write("\nНажмите любую кнопку");
-Console.ReadKey();
+static void theory()
+{
+	Console.Clear();
+	throw new NotImplementedException("theory()");
+}
+
+static void benchmarks()
+{
+	var benchmarks = Benchmarks.BuildNetworks(10_000);
+	Console.WriteLine("Сети построены");
+
+	benchmarks.RunBenchmarks(100_000);
+	Console.Write("\nНажмите любую кнопку");
+	Console.ReadKey();
+	Console.Clear();
+}
+
+static void main()
+{
+	benchmarks();
+}
+
+main();
