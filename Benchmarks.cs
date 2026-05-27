941029e78e44e2493b945699eb3060b8d1a7e318
zhigunovsemyon <zhigunovsemen@gmail.com>
2026-05-27 15:55:32 +0300

    Вынес бенчмарки в свой класс

---
+++ Benchmarks.cs
@@ -0,0 +1,64 @@
+﻿namespace CS_DHT;
+
+using System.Diagnostics;
+using CAN;
+using Chord;
+using Pastry;
+
+public class Benchmarks
+{
+	private readonly ChordSimulator _chord = new();
+	private readonly CANSimulator _can2d = new(2);
+	private readonly CANSimulator _can10d = new(10);
+	private readonly PastrySimulator _pastry = new();
+
+	public static Benchmarks BuildNetworks(int nodeCount)
+	{
+		var bench = new Benchmarks();
+
+		bench._chord.BuildNetwork(nodeCount);
+		bench._can2d.BuildNetwork(nodeCount);
+		bench._can10d.BuildNetwork(nodeCount);
+		bench._pastry.BuildNetwork(nodeCount);
+
+		return bench;
+	}
+
+	public void RunBenchmarks(int queryCount)
+	{ 
+		this._chord.RunBenchmark(queryCount);
+		this._pastry.RunBenchmark(queryCount);
+		this._can2d.RunBenchmark(queryCount);
+		this._can10d.RunBenchmark(queryCount);	
+	}
+
+	public void Avgs(int queryCount)
+	{
+		var sw = Stopwatch.StartNew();
+		double avgHops = this._chord.GetAverageHops(queryCount);
+		sw.Stop();
+		Console.WriteLine($"Chord: {sw.ElapsedMilliseconds}, avg hops: {avgHops}");
+
+
+		sw = Stopwatch.StartNew();
+		avgHops = this._can2d.GetAverageHops(queryCount);
+		sw.Stop();
+		Console.WriteLine($"CAN 2D: {sw.ElapsedMilliseconds}, avg hops: {avgHops}");
+
+
+		sw = Stopwatch.StartNew();
+		avgHops = this._can10d.GetAverageHops(queryCount);
+		sw.Stop();
+		Console.WriteLine($"CAN 10D: {sw.ElapsedMilliseconds}, avg hops: {avgHops}");
+
+
+		sw = Stopwatch.StartNew();
+		avgHops = this._pastry.GetAverageHops(queryCount);
+		sw.Stop();
+		Console.WriteLine($"Pastry: {sw.ElapsedMilliseconds}, avg hops: {avgHops}");
+	}
+	private Benchmarks()
+	{
+		Console.WriteLine("Построение бенчмарков");
+	}
+}
--- Program.cs
+++ Program.cs
@@ -1,57 +1,8 @@
-﻿using CS_DHT.CAN;
-using CS_DHT.Pastry;
-using CS_DHT.Chord;
-using System.Diagnostics;
+﻿using CS_DHT;
 
-
-var chord = new ChordSimulator();
-var can2d = new CANSimulator(2);
-var can10d = new CANSimulator(10);
-var pastry = new PastrySimulator();
-
-chord.BuildNetwork(10_000);
-can2d.BuildNetwork(10_000);
-can10d.BuildNetwork(10_000);
-pastry.BuildNetwork(10_000);
-
+var benchmarks = Benchmarks.BuildNetworks(10_000);
 Console.WriteLine("Сети построены");
 
-void benchmarks()
-{
-	chord.RunBenchmark(100_000);
-	pastry.RunBenchmark(100_000);
-	can2d.RunBenchmark(100_000);
-	can10d.RunBenchmark(100_000);
-}
-
-void avgs()
-{
-	var sw = Stopwatch.StartNew();
-	double avgHops = chord.GetAverageHops(100_000);
-	sw.Stop();
-	Console.WriteLine($"Chord: {sw.ElapsedMilliseconds}, avg hops: {avgHops}");
-
-
-	sw = Stopwatch.StartNew();
-	avgHops = can2d.GetAverageHops(100_000);
-	sw.Stop();
-	Console.WriteLine($"CAN 2D: {sw.ElapsedMilliseconds}, avg hops: {avgHops}");
-
-
-	sw = Stopwatch.StartNew();
-	avgHops = can10d.GetAverageHops(100_000);
-	sw.Stop();
-	Console.WriteLine($"CAN 10D: {sw.ElapsedMilliseconds}, avg hops: {avgHops}");
-
-
-	sw = Stopwatch.StartNew();
-	avgHops = pastry.GetAverageHops(100_000);
-	sw.Stop();
-	Console.WriteLine($"Pastry: {sw.ElapsedMilliseconds}, avg hops: {avgHops}");
-}
-
-benchmarks();
-//avgs();
-
-Console.Write("Нажмите любую кнопку");
-Console.ReadLine();
+benchmarks.RunBenchmarks(100_000);
+Console.Write("\nНажмите любую кнопку");
+Console.ReadKey();
