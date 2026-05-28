7b25c6e51c7618b787734fbfbd774029c70d2fe1
zhigunovsemyon <zhigunovsemen@gmail.com>
2026-05-28 21:48:21 +0300

    Вроде сделал класс вопроса

---
+++ Question.cs
@@ -0,0 +1,30 @@
+﻿namespace CS_DHT;
+
+public class Question
+{
+	private static readonly Random _random = new();
+
+	private readonly string[] _answers;
+	private readonly int _rightIdx;
+
+	public IEnumerable<string> Answers => this._answers;
+
+	public string Text { get; init; }
+
+	public Question(string text, int rightIdx, string[] answers)
+	{
+		this.Text = text;
+		var rightAnswer = answers[rightIdx];
+		this._answers = [..answers.OrderBy(_ => _random)];
+
+		for (int i = 0; i < this._answers.Length; i++) {
+			if (rightAnswer == this._answers[i]) {
+				this._rightIdx = i;
+				break;
+			}
+		}
+	}
+
+	bool IsRight(int index) => this._rightIdx == index;
+	
+}
